bcc2adc828d9d7adf818bde35968235daba96c4b
Saurabh Rane <ssrane@asu.edu>
2021-09-27 18:08:44 -0700

    Implemented the getWeatherData() function

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
-
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
   //CODE GOES HERE
+  const weatherPromise = fetch(FULL_URL)
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
 }
 
 /**
